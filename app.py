0b893bf5286d15e35c14bd449f19ed7d8e1383a4
Juan Manuel <57417658+Sputnik98@users.noreply.github.com>
2022-03-24 12:36:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from distutils.log import debug
 from flask import Flask
 from flask import Flask, jsonify
+import math
 import sympy
 
 app = Flask(__name__)
@@ -49,5 +50,38 @@ def funcion(a,dato):
     x = sympy.symbols('x')
     return sympy.sympify(dato).subs(x,a)
 
+#Método de la regla falsa
+@app.route('/reglaFalsa/<string:dato1>/<string:dato2>/<string:dato3>/<string:dato4>')
+def reglaFalse(dato1,dato2,dato3,dato4):
+    inicial = float(dato1)
+    final = float(dato2)
+    tol = float(dato3)
+    dato4 = dato4
+    c = float(0)
+    
+    if(funciones(inicial,dato4)*funciones(final,dato4)>0):
+        reglaFalsa = print('No se cumple la condición')
+        return jsonify({"reglaFalsa" : reglaFalsa})
+    else:
+        while(1>0):
+            c = ((funciones(final,dato4)*inicial) - (funciones(inicial,dato4)*final)) / (funciones(final,dato4)-funciones(inicial,dato4))
+            if(funciones(c,dato4)<=tol):
+                reglaFalsa = c
+                break  
+            elif(funciones(inicial,dato4) * funciones(c,dato4)<0):
+                final = c
+            else:
+                inicial = c
+
+    return jsonify({"reglaFalsa":str(reglaFalsa)})
+
+def funciones(a,datos):
+    x = sympy.symbols('x')
+    return sympy.sympify(datos).subs(x,a)
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=4000)
+    app.run(debug=True, port=5000)
+
